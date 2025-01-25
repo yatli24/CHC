cc33ba02f05b0a7eeac956e276878c53237474be
johnsonsyleung <114114658+johnsonsyleung@users.noreply.github.com>
2025-01-25 14:20:51 -0800

    Update web_scrapper.R

--- web_scrapper.R
+++ web_scrapper.R
@@ -18,6 +18,3 @@ for (i in 1:1){
   new_url = paste(url, links[i], sep="")
   download_file(new_url, output = paste('/data/', links[i], sep=''))
 }
-
-download.file()
-https://data.chc.ucsb.edu/people/laura/DSCapstone_2025/Data/NMME/COLA-RSMAS-CESM1/prec/prec.COLA-RSMAS-CESM1.1991.mon_Apr.nc
